1704807c5bdebcb7b72b9f3e766d5bde24ffb1a6
tmclean85 <trevor.a.mclean@gmail.com>
2017-08-02 09:00:03 -0700

    Finish refactoring to meteor and mongoDB, tweak input functionality and list order

--- imports/start-up/server/fixtures.js
+++ imports/start-up/server/fixtures.js
@@ -1,12 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { ToDos } from '../../api/todos';
+import { Accounts } from 'meteor/accounts-base';
 
 
 Meteor.startup(() => {
+
+  let user = {};
+
+  if ( Meteor.users.find().count() === 0 ) {
+    user = Accounts.createUser({
+      email: 'maddox@mclean.com',
+      password: 'password'
+    });
+  }
+
   if ( ToDos.find().count() === 0 ) {
     ToDos.insert({
       title: 'Learn React',
       complete: false,
+      owner: user
     });
   }
 });
--- imports/ui/containers/App/index.js
+++ imports/ui/containers/App/index.js
@@ -3,113 +3,121 @@ import PropTypes from 'prop-types';
 import './styles.css';
 import { ToDos } from '../../../api/todos';
 import { createContainer } from 'meteor/react-meteor-data';
+import AccountsUIWrapper from '../../components/AccountsUIWrapper';
+
 
 const ToDoItem = ({ todo, toggleComplete, removeToDo }) => (
-    <li>{todo.title}
-      <input
-         type="checkbox"
-         id={todo._id}
-         checked={todo.complete}
-         onChange= {toggleComplete}
-      />
-      <label htmlFor={todo._id}></label>
-      <button onClick={() => removeToDo(todo)}>
-         <i className="fa fa-trash"></i>
-      </button>
-    </li>
+  <li>{todo.title}
+    <input
+      type="checkbox"
+      id={todo._id}
+      checked={todo.complete}
+      onChange={toggleComplete}
+    />
+    <label htmlFor={todo._id}></label>
+    <button onClick={() => removeToDo(todo)}>
+      <i className="fa fa-trash"></i>
+    </button>
+  </li>
 );
 
 const ClearButton = ({ removeCompleted }) => (
-  <button onClick={removeCompleted}>Clear</button>       
+  <button onClick={removeCompleted}>Clear</button>
 );
-   
+
 const ToDoCount = ({ number }) => (
   <p>{number} {number === 1 ? 'todo' : 'todos'}</p>
 );
-
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    
   }
 
   componentDidMount() {
     this.toDoInput.focus();
   }
-
+  
   toggleComplete(item) {
-    const newTodos = this.props.todos.map(todo => {
-      if (item.id === todo._id) {
-        todo.complete = !todo.complete
-      }
-      return todo;
-    });
-    this.setState({ todos: newTodos });
+    ToDos.update(item._id, { $set: { complete: !item.complete } });
   }
-
+  
   removeToDo = (item) => {
-    const newTodos = this.props.todos.filter(todo => todo._id !== item.id);
-    this.setState({ todos: newTodos });    
+    ToDos.remove(item._id)
   }
-
+  
   removeCompleted = () => {
-   let newTodos = this.props.todos.filter((todo) => !todo.complete);
-   this.setState({ todos: newTodos });
+    const todoIds = this.props.todos.filter(todo => todo.complete).map(todo => todo._id);
+    todoIds.forEach(id => ToDos.remove(id));
   }
 
   hasCompleted = () => {
     const complete = this.props.todos.filter((todo) => todo.complete);
-      return !!complete.length;
+    return !!complete.length;
   }
 
   addToDo = (event) => {
     event.preventDefault();
     if (this.toDoInput.value) {
-      ToDos.insert({      
-        title: this.toDoInput.value,     
+      ToDos.insert({
+        owner: this.props.currentUserId,
+        title: this.toDoInput.value,
         complete: false
       });
     }
+    this.toDoInput.value = '';
   }
 
   render() {
     const { todos } = this.props;
 
     return (
-      <div className="todo-list">
-        <h1>Things to Procrastinate On</h1>
-        <div className="add-todo">
-          <form name="addTodo" onSubmit={this.addToDo}>
-            <input type="text" ref={(input) => (this.toDoInput = input)} />
-            <span>(press enter to add)</span>
-          </form>
+      <div className="app-wrapper">
+        <div className="login-wrapper">
+          <AccountsUIWrapper />
+          <div className="todo-list">
+            <h1>Kindly do the following:</h1>
+            {(!this.props.currentUserId) ?
+              <div className="logged-out-message">
+                <p>Please sign in to see your tasks.</p>
+              </div>
+              :
+              <div>
+                <div className="add-todo">
+                  <form name="addTodo" onSubmit={this.addToDo}>
+                    <input type="text" ref={(input) => (this.toDoInput = input)} />
+                    <span>(press enter to add)</span>
+                  </form>
+                </div>
+                <ul className="the-todos">
+                  {this.props.todos.map((todo, i) =>
+                    <ToDoItem toggleComplete={() =>
+                      this.toggleComplete(todo)}
+                      key={todo._id}
+                      todo={todo}
+                      removeToDo={this.removeToDo}
+                    />
+                  ).reverse()}
+                </ul>
+                <div className="todo-admin">
+                  <ToDoCount number={this.props.todos.length} />
+                  {this.hasCompleted() &&
+                    <ClearButton removeCompleted={this.removeCompleted} />
+                  }
+                </div>
+              </div>
+            }
+          </div>
         </div>
-        <ul>  
-          {this.props.todos.map((todo, i) => 
-            <ToDoItem toggleComplete={() => 
-              this.toggleComplete(todo)} 
-              key={todo._id} 
-              todo={todo}
-              removeToDo={this.removeToDo}
-              />
-            )}
-        </ul>         
-        <div className="todo-admin">   
-          <ToDoCount number={this.props.todos.length}/>
-          {this.hasCompleted() &&
-          <ClearButton removeCompleted={this.removeCompleted} />
-        }
-        </div> 
-      </div>   
-    );    
+      </div>
+    );
   }
 }
 
 App.defaultProps = {
   todos: []
-}
+};
 
 ToDoCount.propTypes = {
   number: PropTypes.number.isRequired
@@ -119,20 +127,26 @@ ClearButton.propTypes = {
   removeCompleted: PropTypes.func.isRequired
 };
 
-ToDoItem.propTypes = {
-  todo: PropTypes.shape({
-    _id: PropTypes.string,
-    title: PropTypes.string,
-    complete: PropTypes.bool
-  }).isRequired,
-  toggleComplete: PropTypes.func.isRequired,
-  removeToDo: PropTypes.func.isRequired
+App.propTypes = {
+  todos: PropTypes.array.isRequired,
+  currentUserId: PropTypes.string,
+  currentUser: PropTypes.object,
+
+  // todos: PropTypes.shape({
+  //   _id: PropTypes.string,
+  //   title: PropTypes.string,
+  //   complete: PropTypes.bool
+  // }).isRequired,
+  // toggleComplete: PropTypes.func.isRequired,
+  // removeToDo: PropTypes.func.isRequired
 };
 
 
 
 export default createContainer(() => {
   return {
+    currentUser: Meteor.user(),
+    currentUserId: Meteor.userId(),
     todos: ToDos.find().fetch()
   };
 }, App);
